28593b842253f030674e445cdc61b7ee8fb3afae
prasantjhaonemail-sketch <prasantjhagit@gmail.com>
2026-03-21 14:43:32 +0530

    Sms Trigger

--- SchoolManagement/Controllers/LoginController.cs
+++ SchoolManagement/Controllers/LoginController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using SchoolManagement_Ui.Models.Login;
 using SchoolManagement_Ui.Service.Admin;
 using System.Security.Claims;
+using System.Text;
 
 public class LoginController : Controller
 {
@@ -16,12 +17,42 @@ public class LoginController : Controller
     [HttpGet]
     public IActionResult Login()
     {
+
+
+
         return View();
     }
 
     [HttpPost]
     public async Task<IActionResult> Login(LoginModel model)
     {
+        //string apiKey = "cWy83ipk0iWWaieCAYYpd3lwhgjLqXV4q2jPD73rL5IRFc8CekQl7NjxKnwD"; // paste your key
+
+        //using (HttpClient client = new HttpClient())
+        //{
+        //    client.DefaultRequestHeaders.Clear();
+        //    client.DefaultRequestHeaders.Add("authorization", apiKey);
+
+        //    var json = @"{
+        //        ""route"": ""q"",
+        //        ""message"": ""Your OTP is 2321"",
+        //        ""language"": ""english"",
+        //        ""numbers"": ""919234652921""
+        //    }";
+
+        //    var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        //    var response = await client.PostAsync("https://www.fast2sms.com/dev/bulkV2", content);
+        //    var result1 = await response.Content.ReadAsStringAsync();
+
+        //    Console.WriteLine("Status: " + response.StatusCode);
+        //    //Console.WriteLine("Response: " + result);
+        //}
+    
+
+
+
+
         if (!ModelState.IsValid)
         {
             return View(model);
